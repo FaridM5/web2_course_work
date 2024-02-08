240f6c42f04b0dc0174611dd2962c9b0baf2ccd5
Farid Mammadli <fmammadli12101@ada.edu.az>
2024-02-08 14:39:18 +0400

    Gradle app created!

---
+++ Week02/gradle_java_standalone/src/main/java/com/ada/gradle_app.java
@@ -0,0 +1,7 @@
+package com.ada;
+
+public class gradle_app{
+    public static void main(String[] args){
+        System.out.println("This is a Gradle APP!");
+    }
+}
---
+++ Week02/gradle_java_standalone/src/test/java/com/ada/gradle_appTest.java
@@ -0,0 +1,10 @@
+package com.ada;
+
+import org.junit.Test;
+
+public class gradle_appTest{
+    @Test
+    public void verifyNoExceptionThrown() {
+        gradle_app.main(new String[]{});
+    }
+}
