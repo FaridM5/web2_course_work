867ee7ff82018f5b5333e56eded1170403bb887e
Farid Mammadli <fmammadli12101@ada.edu.az>
2024-02-22 16:44:20 +0400

    class activity(with errors)

--- Week05/first-spring-app/src/main/java/com/ada/edu/wm2/firstspringapp/FirstSpringAppApplication.java
+++ Week05/first-spring-app/src/main/java/com/ada/edu/wm2/firstspringapp/FirstSpringAppApplication.java
@@ -1,7 +1,9 @@
 package com.ada.edu.wm2.firstspringapp;
 
+import com.ada.edu.wm2.firstspringapp.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- Week05/first-spring-app/src/main/java/com/ada/edu/wm2/firstspringapp/controller/PersonController.java
+++ Week05/first-spring-app/src/main/java/com/ada/edu/wm2/firstspringapp/controller/PersonController.java
@@ -1,10 +1,14 @@
 package com.ada.edu.wm2.firstspringapp.controller;
 
 import com.ada.edu.wm2.firstspringapp.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import  org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -12,15 +16,37 @@ import java.util.List;
 @Controller
 @RequestMapping("/persons")
 public class PersonController {
+    this.defaultPerson = defaultPerson;
+    this.secondPerson = seconPerson;
+    private List personsList = new ArrayList(List.of(
+            defaultPerson, secondPerson,
+            new Person(1,"Ali","Aliyev"),
+            new Person(1,"Manaf", "Aghazade"),
+            new Person(1,"Gultaj", "Aliyev")));
+
+    @Primary
+    @Autowired
+    private Person defaultPerson;
+
+    @Autowired
+    private final Person secondPerson;
+
     @GetMapping
     public  String getAllPersons(Model model){
 
-        var list = List.of(
-                new Person(1,"Ali","Aliyev"),
-                new Person(1,"Manaf", "Aghazade"),
-                new Person(1,"Gultaj", "Aliyev"));
-
         model.addAttribute("persons", list);
-        return "persons";
+        return "person_list";
     }
+
+    @GetMapping("/new")
+    public String showNewForm(Model model){
+        model.addAttribute("person", defaultPerson);
+        return "new_person_form";
+    }
+    @PostMapping("/save")
+    public String savePerson(@ModelAttribute("person") Person newPerson){
+        this.personsList.add(newPerson);
+        return "redirect:/persons";
+    }
+
 }
